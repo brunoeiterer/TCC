8f378e92dfea29297156b8db99ce2fa1e4a9cb67
Unknown <brunoeiterer@gmail.com>
2017-12-01 08:55:45 -0200

    Add presentation

---
+++ presentation/plot_figures.m
@@ -0,0 +1,9 @@
+measuredCurve15A = csvread('solarMeasuredCurve1-5A.csv'); % read 1.5 A measured curve
+measuredCurve5A = csvread('solarMeasuredCurve5A.csv'); % read 5 A measured curve
+measuredCurve85A = csvread('solarMeasuredCurve8-5A.csv'); % read 8.5 A measured curve
+
+modelCurve15A = csvread('solarModelCurve1-5A.csv');  % read 1.5 A model curve
+modelCurve5A = csvread('solarModelCurve5A.csv');  % read 5 A model curve
+modelCurve85A = csvread('solarModelCurve8-5A.csv');  % read 8.5 A model curve
+
+
